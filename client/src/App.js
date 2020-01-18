0957bdab5e01342c91da22c0d4679f3de404850a
HoussamBenhadine <houssambenhadine21@gmail.com>
2020-01-18 14:38:51 +0100

    implement redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavBar from '../src/components/AppNavBar';
 import AppHome from '../src/components/AppHome';
 import AppFooter from '../src/components/AppFooter';
 
-export function App () {
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from "./actions/authActions";
 
-    
+
+class App extends Component {
+
+    componentDidMount() {
+        store.dispatch(loadUser());
+    }
+    render() {
         return (
-            <>
-            <AppNavBar/>
-            <AppHome />
-            <AppFooter />
-            </>
+            <Provider store={store}>
+                <>
+                    <AppNavBar />
+                    <AppHome />
+                    <AppFooter />
+                </>
+            </Provider>
+
         );
-    
+    }
+
+
 }
 
-  
+export default App;
+
+
 
