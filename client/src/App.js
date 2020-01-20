c357a6953b0e6213c26680fb8bd9c0e7ba4f0e26
HoussamBenhadine <houssambenhadine21@gmail.com>
2020-01-20 15:01:32 +0100

    landing page and user logged page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavBar from '../src/components/AppNavBar';
-import AppHome from '../src/components/AppHome';
 import AppFooter from '../src/components/AppFooter';
-
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from "./actions/authActions";
@@ -19,7 +17,6 @@ class App extends Component {
             <Provider store={store}>
                 <>
                     <AppNavBar />
-                    <AppHome />
                     <AppFooter />
                 </>
             </Provider>
